aab8a2a8ceed87d19b16ddb2e7d9d73ad35ad6e6
maheshReddy-dev18 <blackpearl9423@gmail.com>
2025-04-10 12:04:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download('punkt')  # ← this line is missing in your code
 
 
 ps = PorterStemmer()
